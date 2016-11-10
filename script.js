4ffc130710226a5e9855b173bae9117217ed7998
Kyle Ziolkowski <kyleziolkowski90@gmail.com>
2016-11-10 09:04:50 -0500

    LAB9

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+	$("li").mouseenter(function(){
+
+	$("li").fadeTo("slow",0.25); 
+
+	}); 
+
+	$("li").mouseleave(function(){
+		$("li").fadeTo("slow", 1);
+
+	}); 
+	
+	$("li").click(function(){
+		$(this).li{
+			'background-color':"blue"
+	});
+
+	$("li").click(function(){
+		$(this).li{
+			
+		}
+
+
+	});
+
+});
